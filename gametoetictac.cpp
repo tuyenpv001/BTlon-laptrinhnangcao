92ae181ad2b548aa62e76601c699ec5b8dadc9e6
tuyenpv001 <tuyenpv2703@gmail.com>
2020-06-15 19:54:24 +0700

    first commit

--- gametoetictac.cpp
+++ gametoetictac.cpp
@@ -7,17 +7,19 @@
 char background [10]= {'o','1','2','3','4','5','6','7','8','9'};
 
 // Khai bao nguyen mau ham
-int xet_win();
 void ban_co();
 void chay_game();
+int xet_win();
 
 int main(){
 	int lua_chon;
 	system("color 4E");
-	printf("\n\n\n\t\t\t\t\t\tGAME TIC TAC TOE\n\n\t\t\t\t\t\t1.BAT DAU\n\t\t\t\t\t\t2.THOAT\n\n");
-	printf("\t\t\t\t\t\tNhap lua chon:  ");
-	scanf("%d",&lua_chon);
-		do{
+	do {
+		system("cls");
+		printf("\n\n\n\t\t\t\t\t\tGAME TIC TAC TOE\n\n\t\t\t\t\t\t1.BAT DAU\n\t\t\t\t\t\t2.THOAT\n\n");
+		printf("\t\t\t\t\t\tNhap lua chon:  ");
+		scanf("%d",&lua_chon);
+		//do{
 			switch (lua_chon){
 				case 1:
 					system("cls");
@@ -34,8 +36,9 @@ int main(){
 					break;
 				case 2:
 					exit(0);	
-				}
-		}while(1);
+			}
+	}while(1);
+		
 	return 0;
 }
 
@@ -56,40 +59,7 @@ void ban_co(){
     printf("\t\t\t\t\t\t     |     |     \n\n");
 }
 
- // Ham xet dieu kien de thang 
-int xet_win(){
-	if (background[1]== background [2] && background [2]==background[3])
-		return 1;
-	
-	else if (background [4]==background [5]&& background [5]==background [6])
-		return 1;
-		
-	else if(background [7]==background [8]&& background [8]==background [9])
-		return 1;
-	
-	else if(background [1]==background [4] && background [4]==background[7])
-		return 1;
-	
-	else if (background[2]==background[5]&&background [5]==background[8])
-		return 1;
-	
-	else if(background[3]==background[6]&&background [6]==background[9])
-		return 1;
-	
-	else if(background[1]==background[5]&&background[5]==background[9])
-		return 1;
-	
-	else if(background[3]==background[5]&&background[5]==background[9])
-		return 1;
-	
-	else if(background[1]!='1'&&background [2]!='2'&& background[3]!='3'&&background[4]!='4'
-	&&background[5]!='5'&&background[6]!='6'&&background[7]!='7'&&background[8]!='8'&&background[9]!=9)
-		return 0;
-	
-	else {
-		return -1;
-	}
-}
+
 void chay_game(){
 	int play=1,i,o_chon;
 	char X_O;
@@ -146,5 +116,40 @@ void chay_game(){
 			printf("\n\t\t\t\t\t\t  Game hoa!!!");
 		}
 		getch();
+}
+
+ // Ham xet dieu kien de thang 
+int xet_win(){
+	if (background[1]== background [2] && background [2]==background[3])
+		return 1;
+	
+	else if (background [4]==background [5]&& background [5]==background [6])
+		return 1;
+		
+	else if(background [7]==background [8]&& background [8]==background [9])
+		return 1;
+	
+	else if(background [1]==background [4] && background [4]==background[7])
+		return 1;
+	
+	else if (background[2]==background[5]&&background [5]==background[8])
+		return 1;
+	
+	else if(background[3]==background[6]&&background [6]==background[9])
+		return 1;
+	
+	else if(background[1]==background[5]&&background[5]==background[9])
+		return 1;
+	
+	else if(background[3]==background[5]&&background[5]==background[7])
+		return 1;
+	
+	else if(background[1]!='1'&&background [2]!='2'&& background[3]!='3'&&background[4]!='4'
+	&&background[5]!='5'&&background[6]!='6'&&background[7]!='7'&&background[8]!='8'&&background[9]!=9)
+		return 0;
+	
+	else {
+		return -1;
+	}
 }
 
